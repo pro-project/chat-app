096bffef531f36624baea6ec704213c4d4f3c47d
dhrubesh <dhrubesh97@gmail.com>
2017-06-15 21:15:33 +0000

    basic socket.io set-up

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io').listen(server)
 
+users = []
+connection = []
 
 app.get('/', function (req, res) {
-  res.send('all set')
+  res.sendFile(__dirname + '/public/index.html')
 })
 
-app.listen(8080, function () {
-  console.log('listening on port 8080....')
-})
+server.listen(8080)
+console.log('listening on port 8080....')
+
+// app.listen(8080, function () {
+//   console.log('listening on port 8080....')
+// })
